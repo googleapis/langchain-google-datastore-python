5edc634ff1a67c3a34d9c24ff1b66a839d6fafe5
Averi Kitsch <akitsch@google.com>
2024-03-06 09:23:24 -0800

    fix: update user agent

--- src/langchain_google_datastore/document_loader.py
+++ src/langchain_google_datastore/document_loader.py
@@ -30,8 +30,8 @@ from .document_converter import (
 )
 from .version import __version__
 
-USER_AGENT_LOADER = "langchain-google-datastore-python:document_loader" + __version__
-USER_AGENT_SAVER = "langchain-google-datastore-python:document_saver" + __version__
+USER_AGENT_LOADER = "langchain-google-datastore-python:document_loader/" + __version__
+USER_AGENT_SAVER = "langchain-google-datastore-python:document_saver/" + __version__
 WRITE_BATCH_SIZE = 500
 
 if TYPE_CHECKING:
